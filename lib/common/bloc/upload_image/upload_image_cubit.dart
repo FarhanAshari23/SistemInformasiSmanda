77ab1cdc84d78d639932ec495e24b3262a634878
Muhammad Farhan Ashari <farhanashari2346@gmail.com>
2026-01-26 21:02:07 +0700

    Fix image save path to include file extension
    Updated the image saving logic in UploadImageCubit to ensure the file extension is included in the saved file name. Also incremented the app version in pubspec.yaml.

--- lib/common/bloc/upload_image/upload_image_cubit.dart
+++ lib/common/bloc/upload_image/upload_image_cubit.dart
@@ -49,9 +49,9 @@ class UploadImageCubit extends Cubit<UploadImageState> {
       }
 
       final File compressedFile = await compressImage(file);
-
+      final customName = "$filename$ext";
       final appDir = await getApplicationDocumentsDirectory();
-      final savePath = p.join(appDir.path, filename);
+      final savePath = p.join(appDir.path, customName);
       final savedFile = await compressedFile.copy(savePath);
       emit(UploadImageSuccess(savedFile));
     } catch (e) {
