f50605ecd17e35d5017f2630be32cbb983932372
Muhammad Farhan Ashari <farhanashari2346@gmail.com>
2026-04-18 14:34:18 +0700

    change logic empty schedule in profile teacher view

--- lib/presentation/profile/views/profile_teacher_schedule_view.dart
+++ lib/presentation/profile/views/profile_teacher_schedule_view.dart
@@ -30,7 +30,106 @@ class ProfileTeacherScheduleView extends StatelessWidget {
           );
         }
         if (state is GetScheduleTeacherLoaded) {
-          if (state.teacherSchedule.isEmpty) {
+          List<DayEntity> scheduleByDay = state.teacherSchedule
+              .where((element) => element.day == hari)
+              .toList();
+          scheduleByDay.sort(
+            (a, b) {
+              final startA = parseTimeSchedule(a.startTime!);
+              final startB = parseTimeSchedule(b.startTime!);
+              return startA.compareTo(startB);
+            },
+          );
+          if (scheduleByDay.isEmpty) {
+            return const Center(child: Text("Tidak mengajar di hari ini"));
+          } else {
+            return ListView.separated(
+              scrollDirection: Axis.vertical,
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+              itemBuilder: (context, index) {
+                if (hari == "Jumat" && index == 0) {
+                  return Padding(
+                    padding: EdgeInsets.only(left: width * 0.85),
+                    child: CustomInkWell(
+                      borderRadius: 8,
+                      defaultColor: AppColors.primary,
+                      onTap: () {
+                        showDialog(
+                          barrierDismissible: false,
+                          context: context,
+                          builder: (context) {
+                            return Dialog(
+                              alignment: Alignment.center,
+                              backgroundColor: Colors.white,
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text(
+                                      "Catatan:",
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w900,
+                                        color: AppColors.primary,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 8),
+                                    const Text(
+                                      Notes.noteFridayTeachers,
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold,
+                                        color: AppColors.primary,
+                                      ),
+                                    ),
+                                    BasicButton(
+                                      onPressed: () => Navigator.pop(context),
+                                      title: "Saya Mengerti",
+                                    )
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                      child: const Center(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(vertical: 8.0),
+                          child: Icon(
+                            Icons.info,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                } else {
+                  final item =
+                      scheduleByDay[hari == "Jumat" ? index - 1 : index];
+                  return CardJadwal(
+                    jam: item.className ?? '',
+                    kegiatan: item.subjectName ?? '',
+                    pelaksana: "${item.startTime} - ${item.endTime}",
+                    urutan: index + (hari == "Jumat" ? 0 : 1),
+                    isTeacher: true,
+                  );
+                }
+              },
+              separatorBuilder: (context, index) => SizedBox(
+                height: height * 0.01,
+              ),
+              itemCount: hari == "Jumat"
+                  ? scheduleByDay.length + 1
+                  : scheduleByDay.length,
+            );
+          }
+        }
+        if (state is GetScheduleTeacherFailure) {
+          if (state.errorMessage ==
+              "Something error: (null):(404):Data jadwal tidak ditemukan") {
             return const Column(
               mainAxisSize: MainAxisSize.min,
               mainAxisAlignment: MainAxisAlignment.center,
@@ -52,107 +151,8 @@ class ProfileTeacherScheduleView extends StatelessWidget {
               ],
             );
           } else {
-            List<DayEntity> scheduleByDay = state.teacherSchedule
-                .where((element) => element.day == hari)
-                .toList();
-            scheduleByDay.sort(
-              (a, b) {
-                final startA = parseTimeSchedule(a.startTime!);
-                final startB = parseTimeSchedule(b.startTime!);
-                return startA.compareTo(startB);
-              },
-            );
-            if (scheduleByDay.isEmpty) {
-              return const Center(child: Text("Tidak mengajar di hari ini"));
-            } else {
-              return ListView.separated(
-                scrollDirection: Axis.vertical,
-                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                itemBuilder: (context, index) {
-                  if (hari == "Jumat" && index == 0) {
-                    return Padding(
-                      padding: EdgeInsets.only(left: width * 0.85),
-                      child: CustomInkWell(
-                        borderRadius: 8,
-                        defaultColor: AppColors.primary,
-                        onTap: () {
-                          showDialog(
-                            barrierDismissible: false,
-                            context: context,
-                            builder: (context) {
-                              return Dialog(
-                                alignment: Alignment.center,
-                                backgroundColor: Colors.white,
-                                child: Padding(
-                                  padding: const EdgeInsets.all(16.0),
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      const Text(
-                                        "Catatan:",
-                                        style: TextStyle(
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.w900,
-                                          color: AppColors.primary,
-                                        ),
-                                      ),
-                                      const SizedBox(height: 8),
-                                      const Text(
-                                        Notes.noteFridayTeachers,
-                                        style: TextStyle(
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.bold,
-                                          color: AppColors.primary,
-                                        ),
-                                      ),
-                                      BasicButton(
-                                        onPressed: () => Navigator.pop(context),
-                                        title: "Saya Mengerti",
-                                      )
-                                    ],
-                                  ),
-                                ),
-                              );
-                            },
-                          );
-                        },
-                        child: const Center(
-                          child: Padding(
-                            padding: EdgeInsets.symmetric(vertical: 8.0),
-                            child: Icon(
-                              Icons.info,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ),
-                    );
-                  } else {
-                    final item =
-                        scheduleByDay[hari == "Jumat" ? index - 1 : index];
-                    return CardJadwal(
-                      jam: item.className ?? '',
-                      kegiatan: item.subjectName ?? '',
-                      pelaksana: "${item.startTime} - ${item.endTime}",
-                      urutan: index + (hari == "Jumat" ? 0 : 1),
-                      isTeacher: true,
-                    );
-                  }
-                },
-                separatorBuilder: (context, index) => SizedBox(
-                  height: height * 0.01,
-                ),
-                itemCount: hari == "Jumat"
-                    ? scheduleByDay.length + 1
-                    : scheduleByDay.length,
-              );
-            }
+            return Text(state.errorMessage);
           }
-        }
-        if (state is GetScheduleTeacherFailure) {
-          return Text(state.errorMessage);
         }
         return Container();
       },
