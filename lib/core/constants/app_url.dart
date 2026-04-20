b84e8018eafdcb4165a3d0ef2f7c08c003d6e3d4
Muhammad Farhan Ashari <farhanashari2346@gmail.com>
2026-04-20 08:44:26 +0700

    add comment for localhost url

--- lib/core/constants/app_url.dart
+++ lib/core/constants/app_url.dart
@@ -1,5 +1,7 @@
 class AppUrl {
-  static const mainRoute = 'http://puskomsmanda.com:804';
+  static const mainRoute = 'http://192.168.18.3:3000';
   static const storageStudent = '$mainRoute/storage/images/students';
   static const storageTeacher = '$mainRoute/storage/images/teachers';
 }
+
+//ip local = http://192.168.18.3:3000
